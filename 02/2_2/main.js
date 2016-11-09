a1721717be3051257bbf99a9e3a2f2519b2d8d81
Довбня Алексей <dovbnya@intaro.ru>
2016-11-09 22:19:14 +0300

    homework part two

---
+++ 02/2_2/main.js
@@ -0,0 +1,36 @@
+function isSomeTrue( source, filterFn ){
+
+    try{
+        if( source.length == 0 ){
+            throw new Error('Массив не должен быть пустым');
+        }
+
+        for( var i = 0; i < source.length; i++ ){
+            if( filterFn( source[i] ) ){
+                return true;
+            }
+        }
+
+        return false;
+
+    }catch( e ){
+        console.log('Ошибка: '+ e.message);
+    }
+
+    return false;
+
+}
+
+function isNumber( val ) {
+    return typeof val === 'number';
+}
+
+var allNumbers = [1, 2, 4, 5, 6, 7, 8],
+    someNumbers = [1, 2, 'привет', 4, 5, 'loftschool', 6, 7, 8],
+    noNumbers = ['это', 'массив', 'без', 'чисел'];
+
+
+
+console.log(isSomeTrue(allNumbers, isNumber)); //вернет true
+console.log(isSomeTrue(someNumbers, isNumber)); //вернет true
+console.log(isSomeTrue(noNumbers, isNumber)); //вернет false
