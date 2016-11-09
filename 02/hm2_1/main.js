cb2840635e8192dfce27549c5b8ef9baa2b5b1e3
Довбня Алексей <dovbnya@intaro.ru>
2016-11-10 00:35:48 +0300

    homework part one edit-1

--- 02/hm2_1/main.js
+++ 02/hm2_1/main.js
@@ -1,6 +1,5 @@
 function isAllTrue( source, filterFn ){
 
-    try{
         if( source.length == 0 ){
             throw new Error('Массив не должен быть пустым');
         }
@@ -13,12 +12,6 @@ function isAllTrue( source, filterFn ){
 
         return true;
 
-    }catch( e ){
-        console.log('Ошибка: '+ e.message);
-    }
-
-    return false;
-
 }
 
 function isNumber( val ) {
@@ -29,10 +22,15 @@ var allNumbers = [1, 2, 4, 5, 6, 7, 8],
     someNumbers = [1, 2, 'привет', 4, 5, 'loftschool', 6, 7, 8],
     noNumbers = ['это', 'массив', 'без', 'чисел'];
 
+try{
+    
+    console.log(isAllTrue(allNumbers, isNumber)); //вернет true
+    console.log(isAllTrue(someNumbers, isNumber)); //вернет false
+    console.log(isAllTrue(noNumbers, isNumber)); //вернет false
+    console.log(isAllTrue([], isNumber)); //Ошибка
 
+}catch( e ){
+    console.log('Ошибка: '+ e.message);
+}
 
-console.log(isAllTrue(allNumbers, isNumber)); //вернет true
-console.log(isAllTrue(someNumbers, isNumber)); //вернет false
-console.log(isAllTrue(noNumbers, isNumber)); //вернет false
-console.log(isAllTrue([], isNumber)); //Ошибка
 
