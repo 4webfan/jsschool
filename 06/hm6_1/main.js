1b4711956c05b9dd510493bfba2bf5576261ddcf
Довбня Алексей <dovbnya@intaro.ru>
2016-11-25 00:13:26 +0300

    homework part 6-1

---
+++ 06/hm6_1/main.js
@@ -0,0 +1,7 @@
+function timer( time ){
+    return new Promise( function( resolve, reject){
+        setTimeout( function(){ resolve() }, time);
+    } );
+}
+
+timer(3000).then(() => console.log('я вывелась через 3 секунды'));
